1ddf014cf6ea320b28c0325badc002d66c4ecf73
manlan <2733284198@qq.com>
2021-04-29 02:04:49 +0800

    laravel-admin学习 1. 列：排序，颜色，字符串截取， 2. 二维码，可以下载，过滤，

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -66,9 +66,70 @@ class ProductController extends AdminController
     {
         $table = new Table(new News());
 
-        $table->column('id', __('Id'));
-        $table->column('title', __('Title'));
-        $table->column('content', __('Content'));
+//        $table->column('id', __('Id'));
+        $table->column('id', __('Id'))->sortable();
+//        $table->column('title', __('Title'));
+
+//        $table->column('title', __('标题'));
+//        $table->column('title')->setAttributes(['style' => 'color:red;']);
+//        $table->column('title')->width(50);
+//        $table->column('title')->help('这一列是...');
+        $table->column('title')->color('#a0a0a0')->sortable();
+
+
+//        $table->column('content', __('Content'));
+//        $table->column('content', __('内容'));
+        // 修改显示输出
+        $table->column('content',__('内容'))->display(function($text) {
+            if (strlen($text) > 5) {
+                return "<font color=red >".mb_substr($text, 0, 5). '...' ."</font>";
+            }else{
+//                return "<font color=red >$text</font>";
+                return "<font color=blue >$text</font>";
+            }
+
+//            return "<font color=red >$content</font>";
+//            return column('content');
+        });
+
+//        $table->column('content')->limit(10)->help('这一列是...');;
+//        $table->column('content')->copyable();
+        $table->column('url')->qrcode();
+//        $table->column('content')->downloadable();
+
+        // 不存的字段列
+        $table->column('完整内容')->display(function () {
+            return $this->title .'-' .$this->content;
+        });
+
+//        $table->column('content')->display(function($userId) {
+//            return User::find($userId)->name;
+//        });
+
+        /*// filter($callback)方法用来设置表格的简单搜索框
+        $table->filter(function ($filter) {
+            // 设置created_at字段的范围查询
+            $filter->between('created_at', 'Created Time')->datetime();
+        });*/
+
+        /* 禁用功能 */
+//        $table->disableRowSelector();
+//        $table->disableExport();
+//        $table->disableFilter();
+        // 禁用：操作，只显示列表，不可以查看，也不可以编辑的。
+
+        // 结合权限来：现在用户的查询权限，
+//        $table->disableActions();
+
+        // 隐藏列
+//        $table->hideColumns('name', 'title', 'desc');
+//        $table->hideColumns('title', 'desc');
+
+        $table->modalForm();
+
+
+        // 默认为每页20条
+        $table->paginate(5);
 
         return $table;
     }
