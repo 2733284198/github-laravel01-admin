e1b618a00571b5487de75c294474994b6eb60f13
manlan <2733284198@qq.com>
2021-04-28 17:27:57 +0800

    laravel-print安装成功了

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,22 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+//    return view('welcome');
+    return view('test1');
 });
+
+// laravel旧的访问方式
+//Route::get('/home/index', 'HomeController@index');
+//Route::get('/hindex', 'HomeController@index');
+
+// laravel8新的访问方式
+Route::get('/hindex', [HomeController::class, 'index']);
+Route::get('/home/index', [HomeController::class, 'index']);
+Route::get('/home/hello', [HomeController::class, 'hello']);
+Route::get('/home/res', [HomeController::class, 'res']);
+Route::get('/home/viewt', [HomeController::class, 'viewt']);
+Route::get('/home/dbtest', [HomeController::class, 'dbtest']);
+Route::get('/home/dbtest2', [HomeController::class, 'dbtest2']);
+Route::get('/home/dbtest3', [HomeController::class, 'dbtest3']);
+// 集合
+Route::get('/home/coll', [HomeController::class, 'coll']);
