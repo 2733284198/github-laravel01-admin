424392a6c5b0f6771a8dc067477af164d2ff5c04
manlan <2733284198@qq.com>
2021-04-29 01:24:52 +0800

    telescope安装

--- app/Admin/Controllers/ProductController.php
+++ app/Admin/Controllers/ProductController.php
@@ -4,19 +4,21 @@ namespace App\Admin\Controllers;
 
 //use Encore\Admin\Http\Controllers\AdminController;
 use App\Http\Controllers\Controller;
+use App\Models\News;
 use Encore\Admin\Form;
 use Encore\Admin\Http\Controllers\AdminController;
 use Encore\Admin\Layout\Content;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Row;
+use Encore\Admin\Show;
 use Encore\Admin\Table;
 use Illuminate\Database\Eloquent\Model;
 
 //class ProductController extends Controller
-class ProductController extends Controller
+class ProductController extends AdminController
 {
 
-    public function index(Content $content)
+    /*public function index(Content $content)
     {
         $content->title('产品');
         $content->description('产品描述');
@@ -37,9 +39,9 @@ class ProductController extends Controller
         });
 
         return $content;
-    }
+    }*/
 
-    public function show(Content $content)
+    /*public function show(Content $content)
     {
         $content->body('hello world');
 
@@ -47,7 +49,18 @@ class ProductController extends Controller
         $content->view('test1', ['data' => 'foo']);
 
         return $content;
-    }
+    }*/
+
+   /* public function index(Content $content)
+    {
+        $table = new Table(new News());
+
+        $table->column('id', __('Id'));
+        $table->column('title', __('Title'));
+        $table->column('content', __('Content'));
+
+        return $table;
+    }*/
 
     protected function table()
     {
@@ -58,5 +71,31 @@ class ProductController extends Controller
         $table->column('content', __('Content'));
 
         return $table;
+    }
+
+    protected function detail($id)
+    {
+        $show = new Show(News::findOrFail($id));
+
+        $show->field('id', __('Id'));
+        $show->field('title', __('Title'));
+        $show->field('content', __('Content'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new News());
+
+        $form->text('title', __('Title'));
+        $form->text('content', __('Content'));
+
+        return $form;
     }
 }
