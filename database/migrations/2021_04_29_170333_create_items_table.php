994a639b231e8be62c859ee23a445ee9f54ee5ed
manlan <2733284198@qq.com>
2021-04-30 01:15:06 +0800

    items_table数据迁移

---
+++ database/migrations/2021_04_29_170333_create_items_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string("name");
+            $table->string("title")->default('title1');
+            $table->string("numbers");
+            $table->enum("status",[1,2]);
+            $table->decimal("money1");
+            $table->decimal("money2",10,4);
+            $table->timestamps();
+
+            $table->engine = 'InnoDB';
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
